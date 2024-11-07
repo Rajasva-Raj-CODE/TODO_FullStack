72851d931eaf4d1e90ee23cbf8112f8b1549e67c
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2024-11-07 19:20:33 +0530

    get,update,delete TODO api done

--- backend/controllers/todo.controllers.js
+++ backend/controllers/todo.controllers.js
@@ -14,4 +14,45 @@ export const createTodo = async (req, res) => {
   }
 };
 
+export const getTodo = async (req, res) => {
+  try {
+    const todo = await TODO.find({});
+    if (!todo) {
+      return res.status(404).json({ sucess: false, message: "No Todos found" });
+    }
+    res.status(200).json({ sucess: true, message: "Todos found", todo });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ sucess: false, message: "Something went wrong" });
+  }
+};
 
+export const updateTodo = async (req, res) => {
+  try {
+    const todoId = req.params.id;
+    const updatedTodo = await TODO.findByIdAndUpdate(todoId, req.body, {
+      new: true,
+    });
+    res
+      .status(200)
+      .json({
+        sucess: true,
+        message: "Todos udpated successfully",
+        updatedTodo,
+      });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ sucess: false, message: "Something went wrong" });
+  }
+};
+
+export const deleteTodo = async (req, res) => {
+    try {
+        const todoId = req.params.id;
+        const deletedTodo = await TODO.findByIdAndDelete(todoId);
+        res.status(200).json({ sucess: true, message: "Todo deleted successfully", deletedTodo });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ sucess: false, message: "Something went wrong" });
+    }
+}
--- backend/routers/todo.router.js
+++ backend/routers/todo.router.js
@@ -1,10 +1,12 @@
 import express from "express";
-import {createTodo} from "../controllers/todo.controllers.js"
+import {createTodo,getTodo,updateTodo,deleteTodo} from "../controllers/todo.controllers.js";
 
 
 const router = express.Router();
 
 router.post("/create",createTodo)
-
+router.get("/fetch",getTodo)
+router.put("/update/:id",updateTodo)
+router.delete("/delete/:id",deleteTodo)
 
 export default router;
